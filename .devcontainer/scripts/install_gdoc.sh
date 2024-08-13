f90c5be2d04bb448cc73cb656437f89b6b9c7a7f
Rachel Orey <60627889+rachelorey@users.noreply.github.com>
2024-08-13 14:23:40 +0000

    Aug 13, 2024, 2:23 PM

--- .devcontainer/scripts/install_gdoc.sh
+++ .devcontainer/scripts/install_gdoc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 code --install-extension vsls-contrib.gitdoc
+code --command "GitDoc: Enable"
--- .devcontainer/scripts/start-autocommit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run autocommit.sh in a loop
-while true; do
-    sleep 300 # runs every 5 min
-    .devcontainer/scripts/autocommit.sh 
-done &
