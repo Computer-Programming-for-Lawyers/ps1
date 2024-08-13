0616146ea8d229ac40b8fec47fc9a4201bdc9244
Rachel Orey <60627889+rachelorey@users.noreply.github.com>
2024-08-13 14:25:00 +0000

    Aug 13, 2024, 2:25 PM

--- .devcontainer/scripts/install_gdoc.sh
+++ .devcontainer/scripts/install_gdoc.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 code --install-extension vsls-contrib.gitdoc
 code --command "GitDoc: Enable"
+
+
+# Adjust GitDoc to auto-commit every 5 minutes
+settings_path="$HOME/.config/Code/User/settings.json"
+
+# Ensure settings.json exists
+mkdir -p "$(dirname "$settings_path")"
+touch "$settings_path"
+
+# Add or update the autoCommitDelay setting
+jq '.["gitdoc.autoCommitDelay"] = 300' "$settings_path" > tmp.$$.json && mv tmp.$$.json "$settings_path"
